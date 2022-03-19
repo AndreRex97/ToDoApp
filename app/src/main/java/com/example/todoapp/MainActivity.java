2dc170cb06a823bd8c3306bc7aa882a0b7679f1a
Andre Rexdian Archie <andrerexdianarchie@gmail.com>
2022-03-19 15:57:21 +0800

    Modify test variable value

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String test = "test again";
+        String test = "test again and again";
     }
 }
