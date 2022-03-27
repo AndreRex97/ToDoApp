9dd00a7e697c6c0a93db9261f31a374759d4ca87
Jarel Soo <jarelsoo1992@gmail.com>
2022-03-27 20:39:50 +0800

    Added - Finished AddTaskTest and added DeleteTAskTest.

--- app/src/androidTest/java/com/example/todoapp/DeleteTaskTest.java
+++ app/src/androidTest/java/com/example/todoapp/DeleteTaskTest.java
@@ -1,5 +1,4 @@
 package com.example.todoapp;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.junit.Before;
@@ -9,6 +8,10 @@ import org.junit.runner.RunWith;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.action.GeneralLocation;
+import androidx.test.espresso.action.GeneralSwipeAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Swipe;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -24,10 +27,12 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -36,9 +41,8 @@ import static org.hamcrest.core.IsNot.not;
 
 import android.os.SystemClock;
 import android.view.View;
-
 @LargeTest
-public class AddTaskTest {
+public class DeleteTaskTest {
     private String taskName;
     private String taskDescription;
 
@@ -46,29 +50,19 @@ public class AddTaskTest {
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void initValidString(){
+    public void initValidString() {
         taskName = "Test3";
         taskDescription = "Test Task Description";
     }
 
 
     @Test
-    public void create_task() throws InterruptedException {
-        //Enter task name
-        onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.newTaskText))
-                .perform(typeText(taskName),closeSoftKeyboard());
-        // Click on new task button
-        onView(withId(R.id.newTaskButton)).perform(click());
+    public void delete_task() throws InterruptedException {
 
-        // Check that task is submitted successfully
-        SystemClock.sleep(5000);
-        onView(ViewMatchers.withId(R.id.tasksRecyclerView))
-                // scrollTo will fail the test if no item matches.
-                .perform(RecyclerViewActions.scrollTo(
-                        hasDescendant(withText(taskName))
-                ));
+        onView(withId(R.id.tasksRecyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
+                        Swipe.SLOW, GeneralLocation.BOTTOM_RIGHT, GeneralLocation.BOTTOM_LEFT,
+                        Press.FINGER)));
+
     }
-
-
 }
