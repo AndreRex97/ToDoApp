fb72feed13aa450503f527c61a1b9c8d4dd4b879
Jarel Soo <jarelsoo1992@gmail.com>
2022-04-05 18:18:30 +0800

    Added - Updated remaining testing for remaining functions, and one new test for tutorial page.

--- app/src/androidTest/java/com/example/todoapp/EditTaskTest.java
+++ app/src/androidTest/java/com/example/todoapp/EditTaskTest.java
@@ -2,6 +2,7 @@ package com.example.todoapp;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -34,6 +36,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -42,37 +45,159 @@ import static org.hamcrest.core.IsNot.not;
 
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.DatePicker;
 
 public class EditTaskTest {
 
-    private String taskName;
-    private String taskDescription;
+    private String editTaskName;
+    private int TC008_month;
+    private int TC008_day;
+    private int TC008_year;
+    private int TC009_month;
+    private int TC009_day;
+    private int TC009_year;
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
     public void initValidString() {
-        taskName = "Test3";
-        taskDescription = "Test Task Description";
+        editTaskName = "Edit Task 1";
+        TC008_month = 3;
+        TC008_day = 30;
+        TC008_year = 2022;
+
+        TC009_month = 3;
+        TC009_day = 31;
+        TC009_year = 2022;
     }
 
     @Test
-    public void edit_task() throws InterruptedException {
-        //Swipe left to delete
+    public void TC005() throws InterruptedException {
+        //Swipe right to edit
         onView(withId(R.id.tasksRecyclerView)).perform(
                 RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
                         Swipe.SLOW, GeneralLocation.BOTTOM_LEFT, GeneralLocation.BOTTOM_RIGHT,
                         Press.FINGER)));
         SystemClock.sleep(2000);
-        onView(withId(R.id.newTaskText)).perform(replaceText(taskName),closeSoftKeyboard());
+        onView(withId(R.id.newTaskText)).perform(replaceText(editTaskName),closeSoftKeyboard());
 
         onView(withId(R.id.newTaskButton)).perform(click());
 
         onView(ViewMatchers.withId(R.id.tasksRecyclerView))
                 // scrollTo will fail the test if no item matches.
                 .perform(RecyclerViewActions.scrollTo(
-                        hasDescendant(withText(taskName))
+                        hasDescendant(withText(editTaskName))
                 ));
+
+        SystemClock.sleep(3000);
+        //onView(withId(R.id.newTaskButton)).perform(click());
+    }
+
+    @Test
+    public void TC006() throws InterruptedException {
+        //Swipe right to edit
+        onView(withId(R.id.tasksRecyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
+                        Swipe.SLOW, GeneralLocation.BOTTOM_LEFT, GeneralLocation.BOTTOM_RIGHT,
+                        Press.FINGER)));
+        SystemClock.sleep(2000);
+        onView(withId(R.id.newTaskText)).perform(replaceText(editTaskName),closeSoftKeyboard());
+
+        //Add image code here
+
+        onView(withId(R.id.newTaskButton)).perform(click());
+
+        onView(ViewMatchers.withId(R.id.tasksRecyclerView))
+                // scrollTo will fail the test if no item matches.
+                .perform(RecyclerViewActions.scrollTo(
+                        hasDescendant(withText(editTaskName))
+                ));
+
+        SystemClock.sleep(5000);
+        //onView(withId(R.id.newTaskButton)).perform(click());
+    }
+
+    @Test
+    public void TC007() throws InterruptedException {
+        //Swipe right to edit
+        onView(withId(R.id.tasksRecyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
+                        Swipe.SLOW, GeneralLocation.BOTTOM_LEFT, GeneralLocation.BOTTOM_RIGHT,
+                        Press.FINGER)));
+        SystemClock.sleep(2000);
+        onView(withId(R.id.newTaskText)).perform(replaceText(editTaskName),closeSoftKeyboard());
+
+        //Add image code here
+
+        onView(withId(R.id.newTaskButton)).perform(click());
+
+        onView(ViewMatchers.withId(R.id.tasksRecyclerView))
+                // scrollTo will fail the test if no item matches.
+                .perform(RecyclerViewActions.scrollTo(
+                        hasDescendant(withText(editTaskName))
+                ));
+
+        SystemClock.sleep(5000);
+        //onView(withId(R.id.newTaskButton)).perform(click());
+    }
+
+    @Test
+    public void TC008() throws InterruptedException {
+        //Swipe right to edit
+        onView(withId(R.id.tasksRecyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
+                        Swipe.SLOW, GeneralLocation.BOTTOM_LEFT, GeneralLocation.BOTTOM_RIGHT,
+                        Press.FINGER)));
+        SystemClock.sleep(2000);
+
+
+        //Click on set due date button
+        onView(withId(R.id.datePickerButton))
+                .perform(click());
+
+        SystemClock.sleep(2000);
+
+        //select date
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(TC008_year, TC008_month, TC008_day));
+        //Click OK button after selecting date
+        onView(withText("OK"))
+                .inRoot(isDialog()) // <---
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        // Click on save button
+        onView(withId(R.id.newTaskButton)).perform(click());
+
+        SystemClock.sleep(5000);
+        //onView(withId(R.id.newTaskButton)).perform(click());
+    }
+
+    @Test
+    public void TC009() throws InterruptedException {
+        //Swipe right to edit
+        onView(withId(R.id.tasksRecyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
+                        Swipe.SLOW, GeneralLocation.BOTTOM_LEFT, GeneralLocation.BOTTOM_RIGHT,
+                        Press.FINGER)));
+        SystemClock.sleep(2000);
+
+
+        //Click on set due date button
+        onView(withId(R.id.datePickerButton))
+                .perform(click());
+
+        SystemClock.sleep(2000);
+
+        //select date
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(TC009_year, TC009_month, TC009_day));
+        //Click OK button after selecting date
+        onView(withText("OK"))
+                .inRoot(isDialog()) // <---
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        // Click on save button
+        onView(withId(R.id.newTaskButton)).perform(click());
 
         SystemClock.sleep(5000);
         //onView(withId(R.id.newTaskButton)).perform(click());
--- app/src/androidTest/java/com/example/todoapp/TutorialPageTest.java
+++ app/src/androidTest/java/com/example/todoapp/TutorialPageTest.java
@@ -1,6 +1,8 @@
 package com.example.todoapp;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -8,63 +10,90 @@ import org.junit.runner.RunWith;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.CoordinatesProvider;
+import androidx.test.espresso.action.GeneralClickAction;
 import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
+import androidx.test.espresso.action.Tap;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
+import androidx.test.runner.intent.IntentCallback;
+import androidx.test.runner.intent.IntentMonitorRegistry;
 
 import com.example.todoapp.MainActivity;
 import com.example.todoapp.R;
+import com.google.android.apps.common.testing.accessibility.framework.replacements.Uri;
 
+import static androidx.test.InstrumentationRegistry.getTargetContext;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.EasyMock2Matchers.equalTo;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsNot.not;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.view.InputDevice;
+import android.view.MotionEvent;
 import android.view.View;
-@LargeTest
-public class DeleteTaskTest {
+import android.widget.DatePicker;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class TutorialPageTest {
+
+    private String TC0011_TutorialText;
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void initValidString() {
+    public void initValidString(){
+        TC0011_TutorialText = "Tutorial";
 
     }
 
-
     @Test
-    public void TC010() throws InterruptedException {
-        //Swipe left to delete
-        onView(withId(R.id.tasksRecyclerView)).perform(
-                RecyclerViewActions.actionOnItemAtPosition(0, new GeneralSwipeAction(
-                        Swipe.SLOW, GeneralLocation.BOTTOM_RIGHT, GeneralLocation.BOTTOM_LEFT,
-                        Press.FINGER)));
+    public void TC0011() throws InterruptedException {
+        //Click on tutorial button
+        onView(withId(R.id.buttonTutorial)).perform(click());
+        SystemClock.sleep(1000);
+        //Check that user landed on tutorial page
+        onView(withId(R.id.textView))
+                .check(matches(withText(TC0011_TutorialText)));
 
-        //Click on confirm button to delete
-        onView(withText("CONFIRM"))
-       .inRoot(isDialog()) // <---
-                .check(matches(isDisplayed()))
-                .perform(click());
     }
 }
